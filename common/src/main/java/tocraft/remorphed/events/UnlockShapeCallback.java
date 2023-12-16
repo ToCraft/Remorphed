a8cc72b677fe5149a4bb04c7619159c618f21f99
ToCraft <piminecraft492@gmail.com>
2023-12-16 18:20:08 +0100

    prevent walkers native unlocking

--- common/src/main/java/tocraft/remorphed/events/UnlockShapeCallback.java
+++ common/src/main/java/tocraft/remorphed/events/UnlockShapeCallback.java
@@ -2,12 +2,17 @@ package tocraft.remorphed.events;
 
 import net.minecraft.server.level.ServerPlayer;
 import tocraft.craftedcore.events.Event.Result;
+import tocraft.remorphed.impl.RemorphedPlayerDataProvider;
 import tocraft.walkers.api.event.ShapeEvents;
 import tocraft.walkers.api.variant.ShapeType;
 
 public class UnlockShapeCallback implements ShapeEvents.UnlockShapeCallback {
 	@Override
 	public Result unlock(ServerPlayer player, ShapeType<?> type) {
-		return Result.interruptFalse();
+		// check if entity is unlocked by remorphed, prevents native unlocks by walkers
+		if (type != null && !((RemorphedPlayerDataProvider) player).getUnlockedShapes().contains(type))
+			return Result.interruptFalse();
+		else
+			return Result.pass();
 	}
 }
