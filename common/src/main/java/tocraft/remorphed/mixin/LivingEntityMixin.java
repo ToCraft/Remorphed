8eb64a0cd208f30a99929216aa7aae2ea4340c54
ToCraft <piminecraft492@gmail.com>
2023-12-16 18:59:50 +0100

    add config to auto transform into killed mob

--- common/src/main/java/tocraft/remorphed/mixin/LivingEntityMixin.java
+++ common/src/main/java/tocraft/remorphed/mixin/LivingEntityMixin.java
@@ -5,13 +5,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import tocraft.remorphed.Remorphed;
 import tocraft.remorphed.impl.RemorphedPlayerDataProvider;
+import tocraft.walkers.api.PlayerShape;
 import tocraft.walkers.api.variant.ShapeType;
 
 @Mixin(LivingEntity.class)
@@ -23,8 +26,10 @@ public abstract class LivingEntityMixin extends Entity {
     
     @Inject(method = "die", at = @At("HEAD"))
     public void onDeath(DamageSource damageSource, CallbackInfo ci) {
-    	if (!((Object) this instanceof Player) && damageSource.getEntity() instanceof Player killer) {
+    	if (!((Object) this instanceof Player) && damageSource.getEntity() instanceof ServerPlayer killer) {
     		((RemorphedPlayerDataProvider) killer).addUnlockShape(ShapeType.from((LivingEntity) (Object) this));
+    		if (Remorphed.CONFIG.autoTransform)
+    			PlayerShape.updateShapes(killer, ShapeType.from((LivingEntity) (Object) this), ShapeType.from((LivingEntity) (Object) this).create(killer.level()));
     	}
     }
 }
