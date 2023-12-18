1c420927655096c2b697ce826e3479eb0c03a1fe
ToCraft <piminecraft492@gmail.com>
2023-12-18 19:05:47 +0100

    fix empty menu

--- common/src/main/java/tocraft/remorphed/tick/KeyPressHandler.java
+++ common/src/main/java/tocraft/remorphed/tick/KeyPressHandler.java
@@ -1,6 +1,7 @@
 package tocraft.remorphed.tick;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.player.Player;
 import tocraft.craftedcore.events.client.ClientTickEvents;
 import tocraft.remorphed.Remorphed;
 import tocraft.remorphed.RemorphedClient;
@@ -9,16 +10,30 @@ import tocraft.remorphed.screen.RemorphedHelpScreen;
 import tocraft.remorphed.screen.RemorphedScreen;
 
 public class KeyPressHandler implements ClientTickEvents.Client {
+	private boolean canUseShapes = false;
 
 	@Override
 	public void tick(Minecraft client) {
 		assert client.player != null;
 
 		if (RemorphedClient.MENU_KEY.consumeClick()) {
-			if ((Remorphed.CONFIG.lockTransform || ((RemorphedPlayerDataProvider) client.player).getUnlockedShapes().isEmpty()) && !client.player.isCreative())
+			if (!canUseAnyShape(client.player))
 				Minecraft.getInstance().setScreen(new RemorphedHelpScreen());
 			else
 				Minecraft.getInstance().setScreen(new RemorphedScreen());
 		}
+	}
+	
+	private boolean canUseAnyShape(Player player) {
+		canUseShapes = false;
+		
+		((RemorphedPlayerDataProvider) player).getUnlockedShapes().forEach((shape, killAmount) -> {
+			if (killAmount >= Remorphed.CONFIG.killToUnlock) {
+				canUseShapes = true;
+				return;
+			}
+		});
+		
+		return canUseShapes && !Remorphed.transformationIsLocked(player);
 	}
 }
