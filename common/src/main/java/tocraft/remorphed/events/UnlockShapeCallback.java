0e6f966aa02908084f1a6055ceb77406313d221f
ToCraft <piminecraft492@gmail.com>
2024-01-17 21:15:11 +0100

    add config option "unlockFriendlyNormal"

--- common/src/main/java/tocraft/remorphed/events/UnlockShapeCallback.java
+++ common/src/main/java/tocraft/remorphed/events/UnlockShapeCallback.java
@@ -1,8 +1,12 @@
 package tocraft.remorphed.events;
 
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.NeutralMob;
+import net.minecraft.world.entity.TamableAnimal;
 import tocraft.craftedcore.events.Event.Result;
 import tocraft.remorphed.Remorphed;
+import tocraft.remorphed.impl.RemorphedPlayerDataProvider;
 import tocraft.walkers.api.event.ShapeEvents;
 import tocraft.walkers.api.variant.ShapeType;
 
@@ -12,8 +16,12 @@ public class UnlockShapeCallback implements ShapeEvents.UnlockShapeCallback {
         // check if entity is unlocked by remorphed, prevents native unlocks by walkers
         if (!Remorphed.canUseShape(player, type))
             return Result.interruptFalse();
-        else {
-            return Result.pass();
+        else if (Remorphed.CONFIG.unlockFriendlyNormal) {
+            LivingEntity entityToBeUnlocked = type.create(player.level());
+            if (entityToBeUnlocked instanceof TamableAnimal || entityToBeUnlocked instanceof NeutralMob)
+                ((RemorphedPlayerDataProvider) player).remorphed$getUnlockedShapes().put(type, Remorphed.CONFIG.killToUnlock);
         }
+
+        return Result.pass();
     }
 }
