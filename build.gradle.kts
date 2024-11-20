5c2b20bdf77f926a15c30483a862ad5f49dc42e3
To_Craft <development@tocraft.dev>
2024-11-20 14:43:02 +0100

    mark skinshifter as optional in meta data

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ ext {
     modMeta["version"] = version
     modMeta["craftedcore_version"] = project.properties["craftedcore_version"] as String
     modMeta["woodwalkers_version"] = project.properties["woodwalkers_version"] as String
+    modMeta["skinshifter_version"] = project.properties["skinshifter_version"] as String
 
     set("mod_meta", modMeta)
 }
